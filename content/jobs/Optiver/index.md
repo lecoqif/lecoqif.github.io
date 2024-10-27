205eeed8a40427f512296d3b8a2c006ceaa3d699
lecoqif <abhaydais@gmail.com>
2024-10-28 00:28:49 +0100

    Revert "Updates"
    This reverts commit d4a62a49541c03b2ebb8297410a4cae033aedd07.

---
+++ content/jobs/Optiver/index.md
@@ -0,0 +1,13 @@
+---
+date: '2024-10-27'
+title: 'Quantitative Developer'
+company: 'Optiver'
+location: 'Amsterdam, Netherlands'
+range: 'August 2022 - Present'
+url: 'https://www.optiver.com/'
+---
+
+- Designed and implemented the infrastructure for Single Stock Options Spreads desk using DAG architecture (Python, SQL)
+- Implemented research frameworks, and integrated research signals into trading strategies
+- Enhanced monitoring and operational excellence of major production trading systems
+- Improved latency of trading system 15x by modularising components and optimizing architecture
---
+++ src/components/icons/logo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <g transform="translate(-6.000000, -0.000000)">
+        <path
+          d="M 47 25.3 L 60.6917 63 L 54.2917 63 L 51.5 54.5 L 39.5 54.5 L 36.6917 63 L 30.2917 63 L 44 25.3 Z M 40.5 50.5 L 50.5 50.5 L 45.5 35 L 40.5 50.5 Z"
+          fill="currentColor"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
